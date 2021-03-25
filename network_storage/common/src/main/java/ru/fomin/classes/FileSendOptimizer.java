86514e674f68406308a91846243108baf41434d2
Dmitriy Fomin <79067773397@yandex.ru>
2021-03-25 20:39:45 +0300

    new comments for common module

--- network_storage/common/src/main/java/ru/fomin/classes/FileSendOptimizer.java
+++ network_storage/common/src/main/java/ru/fomin/classes/FileSendOptimizer.java
@@ -12,12 +12,20 @@ import java.util.function.Consumer;
 
 import static java.nio.file.Files.*;
 
-
+/**
+ * Class for analysing of file size and choosing method for transfer.
+ */
 public class FileSendOptimizer {
 
     private static final int CHUNK_SIZE = 4 * 1024 * 1024;
 
-
+    /**
+     * Analysing of file size and delegating transfer task to needed method.
+     *
+     * @param path        - path of the file for transfer
+     * @param directoryId - id of directory that this file will be upload
+     * @param sendAction  - action that will be done for transfer DTO
+     */
     public void sendFile(Path path, Long directoryId, Consumer<DataPackage> sendAction) throws IOException {
         if (size(path) < CHUNK_SIZE * 8) {
             sendFull(path, directoryId, sendAction);
@@ -26,27 +34,31 @@ public class FileSendOptimizer {
         }
     }
 
-
+    /**
+     * Transfer file by one DTO.
+     */
     private void sendFull(Path path, Long directoryId, Consumer<DataPackage> sendAction) throws IOException {
         DataPackage pack = new FileDataPackage(path, directoryId);
         sendAction.accept(pack);
     }
 
-
+    /**
+     * Transfer file by several DTO.
+     */
     private void sendByChunks(Path path, Long directoryId, Consumer<DataPackage> sendAction) throws IOException {
         try (InputStream in = newInputStream(path)) {
-            int availCount = in.available();
-            int rem = availCount % CHUNK_SIZE;
+            int availableBytes = in.available();
+            int bytesOfLastPackage = availableBytes % CHUNK_SIZE;
 
             byte[] chunk = new byte[CHUNK_SIZE];
-            byte[] chunkLast = rem != 0 ? new byte[rem] : new byte[CHUNK_SIZE];
+            byte[] chunkLast = bytesOfLastPackage != 0 ? new byte[bytesOfLastPackage] : new byte[CHUNK_SIZE];
 
             int num = 1;
-            while (availCount > CHUNK_SIZE) {
+            while (availableBytes > CHUNK_SIZE) {
                 in.read(chunk);
                 DataPackage pack = new FileChunkPackage(path, chunk, num++, directoryId);
                 sendAction.accept(pack);
-                availCount = in.available();
+                availableBytes = in.available();
             }
 
             in.read(chunkLast);
